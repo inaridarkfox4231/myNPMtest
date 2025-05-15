424e213ec6aa3617ae2aeac582cdaa045ec6b7a6
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2025-05-16 00:02:47 +0900

    npm commit

---
+++ index.js
@@ -0,0 +1,25 @@
+(function(global, factory){
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    //console.log("module is defined");
+    factory(exports);
+  } else if(typeof define === "function" && define.amd) {
+    //console.log("define is defined");
+    define(["exports"], factory);
+  } else {
+    //console.log("global");
+    global = global || self;
+    factory((global.mylibrary = {}));
+  }
+})(this, function (exports) {
+  "use strict";
+
+  function hello(){
+    const message = "my NPM test.";
+    console.log(message);
+    return message;
+  }
+
+  exports.hello = hello;
+
+  Object.defineProperty(exports, "__esModule", { value: true });
+});
